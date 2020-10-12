6970cf79900188912e8efb603fbf5f649799557d
fei.wang <929728742@qq.com>
2020-10-12 10:46:25 +0800

    左右联动列表

---
+++ DoubleListRecyclerView/app/src/main/java/com/kuanquan/doublelistrecyclerview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kuanquan.doublelistrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
