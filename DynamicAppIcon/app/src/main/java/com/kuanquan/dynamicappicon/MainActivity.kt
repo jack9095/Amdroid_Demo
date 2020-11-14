0e26994294608973082717d80cc8bc41453a026d
wangfei <929728742@qq.com>
2020-11-14 15:27:37 +0800

    feature: 动态修改桌面图标

--- DynamicAppIcon/app/src/main/java/com/kuanquan/dynamicappicon/MainActivity.kt
+++ DynamicAppIcon/app/src/main/java/com/kuanquan/dynamicappicon/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.kuanquan.dynamicappicon
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ComponentName
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // 切换图标一
+    fun changeIcon(view: View) {
+        packageManager.setComponentEnabledSetting(
+            componentName,
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+            PackageManager.DONT_KILL_APP
+        )
+        packageManager.setComponentEnabledSetting(
+            ComponentName(this, "$packageName.MainActivity"),
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+            0
+        )
+    }
+
+    // 切换图标二
+    fun changeIconAlias(view: View) {
+        packageManager.setComponentEnabledSetting(
+            componentName,
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+            PackageManager.DONT_KILL_APP
+        )
+        packageManager.setComponentEnabledSetting(
+            ComponentName(this, "$packageName.alias"),
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+            0
+        )
     }
 }
