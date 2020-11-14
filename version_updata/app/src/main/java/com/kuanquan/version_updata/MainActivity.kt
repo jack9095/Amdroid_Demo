4363ef5de2c3e480dab7c808e519ba44591c0b44
wangfei <929728742@qq.com>
2020-11-14 11:26:25 +0800

    feature: 扇形圆形展开收缩菜单，及点赞动画

---
+++ version_updata/app/src/main/java/com/kuanquan/version_updata/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kuanquan.version_updata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
