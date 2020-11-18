be91e22f2bda2bb02fca82ba2cf9616da19f68ac
wangfei <929728742@qq.com>
2020-11-18 20:41:37 +0800

    grade: 多渠道打包

--- MultiChannel/app/src/main/java/com/kuanquan/multichannel/MainActivity.kt
+++ MultiChannel/app/src/main/java/com/kuanquan/multichannel/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.kuanquan.multichannel
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val channel = getMetaData(applicationContext, "APP_CHANNEL")
+        tv_channel_value.text = channel
+        Toast.makeText(applicationContext, BuildConfig.FIELD_TEST, Toast.LENGTH_LONG).show()
+    }
+
+    private fun getMetaData(context: Context, key: String): String? {
+        try {
+            val packageManager = context.packageManager
+            val applicationInfo = packageManager.getApplicationInfo(context
+                    .packageName, PackageManager.GET_META_DATA)
+            return applicationInfo.metaData.getString(key)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return ""
     }
 }
