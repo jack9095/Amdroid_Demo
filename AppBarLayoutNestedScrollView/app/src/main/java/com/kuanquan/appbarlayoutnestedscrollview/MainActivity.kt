d406912cb6df0f90b17600015828ef96898caa70
wangfei <929728742@qq.com>
2020-12-21 20:51:32 +0800

    nested滑动测试

---
+++ AppBarLayoutNestedScrollView/app/src/main/java/com/kuanquan/appbarlayoutnestedscrollview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kuanquan.appbarlayoutnestedscrollview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
