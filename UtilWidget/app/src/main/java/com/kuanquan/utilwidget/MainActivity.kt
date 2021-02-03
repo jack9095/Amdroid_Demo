dd5046de3aadee1ac2172d1ca3a73a67389721f7
wangfei <929728742@qq.com>
2021-02-03 15:09:42 +0800

    feature: 上传到本地 maven 仓库的代码

--- UtilWidget/app/src/main/java/com/kuanquan/utilwidget/MainActivity.kt
+++ UtilWidget/app/src/main/java/com/kuanquan/utilwidget/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.kuanquan.utilwidget
 
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.kuanquan.commentutillibrary.ShapeBuilder
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val drawable = ShapeBuilder()
+            .setCornerRadius(ShapeBuilder.ANGLE_TOP_LEFT, 10f)
+            .setCornerRadius(ShapeBuilder.ANGLE_BOTTOM_LEFT, 10f)
+            .setGradientColors(intArrayOf(Color.parseColor("#000000"), Color.parseColor("#999999"), Color.parseColor("#ffffff")))
+            .setOrientation(GradientDrawable.Orientation.LEFT_RIGHT).build()
+        textView?.background = drawable
     }
 }
--- UtilWidget/commentUtilLibrary/src/main/java/com/kuanquan/commentutillibrary/C.kt
+++ UtilWidget/commentUtilLibrary/src/main/java/com/kuanquan/commentutillibrary/C.kt
@@ -1,5 +1,8 @@
 package com.kuanquan.commentutillibrary
 
+/**
+ * https://blog.csdn.net/xmxkf/article/details/80674232
+ */
 class C {
 
 }
