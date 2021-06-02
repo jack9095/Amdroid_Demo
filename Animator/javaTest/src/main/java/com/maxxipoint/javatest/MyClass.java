d844edd119f65703262c22f8d22ab92125d3d15c
wangfei <929728742@qq.com>
2021-06-02 19:33:06 +0800

    优化 gradle 编译速度

--- Animator/javaTest/src/main/java/com/maxxipoint/javatest/MyClass.java
+++ Animator/javaTest/src/main/java/com/maxxipoint/javatest/MyClass.java
@@ -1,8 +1,23 @@
 package com.maxxipoint.javatest;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class MyClass {
 
     public static void main(String[] args) {
-        System.out.printl("ddddd");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        String inputString = "00:01:50";
+        Date date = null;
+        try {
+            date = sdf.parse("1970-01-01 " + inputString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        System.out.println("in milliseconds: " + date.getTime()/1000);
     }
 }
