b41d7773377a8585e4b1c93dc027f395f514b2d0
wangfei0616 <wangfei0616@shihuo.cn>
2021-11-25 16:15:26 +0800

    Toast工具类

--- ToastUtil/app/src/main/java/com/kuanquan/toastutil/MainActivity.kt
+++ ToastUtil/app/src/main/java/com/kuanquan/toastutil/MainActivity.kt
@@ -13,15 +13,16 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    var mImageView: ImageView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val mImageView = findViewById<ImageView>(R.id.imageView)
-        mImageView.setImageDrawable(R.drawable.shape_gradient.res2Drawable())
-        mImageView.setImageDrawable(R.drawable.shape_gradient.res2Drawable())
+        mImageView = findViewById<ImageView>(R.id.imageView)
     }
 
     fun show1(view: View) {
+        mImageView?.setImageDrawable(R.drawable.shape_gradient.res2Drawable())
         ToastUtils.show(R.string.one_toast.res2String())
     }
     fun show2(view: View) {
@@ -30,19 +31,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
     fun show3(view: View) {
-        ToastUtils.style = WhiteToastStyle()
-        ToastUtils.show("动态切换白色吐司样式", true)
+        ToastUtils.apply {
+            style = WhiteToastStyle()
+            show("动态切换白色吐司样式", true)
+        }
     }
     fun show4(view: View) {
-        ToastUtils.style = BlackToastStyle()
-        ToastUtils.show("动态切换黑色吐司样式")
+        ToastUtils.apply {
+            style = BlackToastStyle()
+            show("动态切换黑色吐司样式")
+        }
+//        ToastUtils.style = BlackToastStyle()
+//        ToastUtils.show("动态切换黑色吐司样式")
     }
     fun show5(view: View) {
-        ToastUtils.setView(R.layout.toast_custom_view)
-        ToastUtils.setGravity(Gravity.CENTER, 0, 0, 0.05f, 0.3f)
-        ToastUtils.show("自定义 Toast 布局", true)
+        ToastUtils.apply {
+            setView(R.layout.toast_custom_view)
+            setGravity(Gravity.CENTER, 0, 0, 0.05f, 0.3f)
+            show("自定义 Toast 布局", true)
+        }
+//        ToastUtils.setView(R.layout.toast_custom_view)
+//        ToastUtils.setGravity(Gravity.CENTER, 0, 0, 0.05f, 0.3f)
+//        ToastUtils.show("自定义 Toast 布局", true)
     }
-    fun show6(view: View) {
 
-    }
 }
